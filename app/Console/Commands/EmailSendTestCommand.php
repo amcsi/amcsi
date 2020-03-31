c40346c747257da0241e96fcf8076f369e04a2c8
Attila Szeremi <attila@szeremi.org>
2020-03-31 18:13:43 +0200

    Get different recipients to get different contents of emails

--- app/Console/Commands/EmailSendTestCommand.php
+++ app/Console/Commands/EmailSendTestCommand.php
@@ -53,11 +53,6 @@ class EmailSendTestCommand extends Command
         // See addDynamicTemplateDatas() lower down to see what you need to do in case of dynamic templates.
         $email->setSubject($subject);
 
-        // Add recipients.
-        foreach ($this->option('to') as $index => $address) {
-            $name = 'Test User ' . ($index + 1); // Assign a name to the user.
-            $email->addTo($address, $name, ['name' => $name]);
-        }
         /* Always have an unsubscribe group, otherwise clicking on the Unsubscribe link (which is mandatory to place)
            would result in a global unsubscribe, preventing the user from receiving such important emails such as
            "forgot password" emails. https://sendgrid.com/docs/ui/sending-email/recipient-subscription-preferences/ */
@@ -77,7 +72,9 @@ class EmailSendTestCommand extends Command
 
         // Set which template to use (unless you want to provide the html yourself).
         $email->setTemplateId(config('services.sendgrid.template_id'));
-        $email->addDynamicTemplateDatas([
+
+        // Template placeholder replacements common to all recipients.
+        $baseTemplateData = [
             'titleText' => 'Dynamic Title Text!',
             'firstParagraph' => 'This is an example paragraph text.',
             'exampleArray' => [
@@ -94,7 +91,43 @@ class EmailSendTestCommand extends Command
             // An example of referencing an attached image with cid.
             // Though it's recommended to just link to an external image, because it's supported in all email clients.
             'image' => "cid:$contentId",
-        ]);
+        ];
+
+        /* Recipients */
+
+        // Add recipients.
+        foreach ($this->option('to') as $index => $address) {
+            // Assign a name to the user for the sake of this test.
+            $name = 'Test User ' . ($index + 1);
+
+            // Merge base template data with recipient-specific template data.
+            // Unfortunately I couldn't find any way to just globally set the base template data, so you'll have to
+            // repeat them for every recipient.
+            $templateData = array_replace($baseTemplateData, ['name' => $name]);
+
+            // Add addressee.
+            $email->addTo(
+                // e.g. "john.doe@example.com"
+                $address,
+                // e.g. "John Doe" to result in "John Doe <john.doe@example.com>".
+                $name,
+
+                // $templateData is only needed if using dynamic SendGrid templates.
+                $templateData,
+
+                // If you are not using per-recipient template data (such as "Dear {{name}}") or you don't mind if
+                // all To/Cc recipients of the email "see each other", then you can leave this argument out.
+                //
+                // Otherwise, by setting this value correctly, different users will get different emails potentially
+                // with different content (depending on template data), and _not_ see each other as recipients.
+                // You need to make sure this personalizationIndex value is unique to each recipient, starting from 0
+                // for the first recipient, and incrementing for each additional recipient. Even if you use a mixture of
+                // To and Cc, the indexes must match the order of adding the recipient to the email.
+                $index
+            );
+
+
+        }
 
         /* Send */
 
